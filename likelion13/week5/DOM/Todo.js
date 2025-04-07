39f550168b4c4575ae8c636a56c95114bfa4def8
xaerinoo <xaerinoo@naver.com>
2025-04-08 03:20:30 +0900

    feat: 이미지 파일 추가 및 버튼에 아이콘 삽입

--- likelion13/week5/DOM/Todo.js
+++ likelion13/week5/DOM/Todo.js
@@ -8,6 +8,22 @@ class Todo {
         this.innerText = new Div(todoText, 'text-box');
         this.completeBtn = new Button('완료', 'complete-btn');
         this.delBtn = new Button('삭제', 'del-btn');
+
+        // 완료 버튼: 아이콘 추가
+        const completeIcon = new Image();
+        completeIcon.src = "./assets/free-icon-check-mark-6520110.PNG";
+        completeIcon.alt = 'Complete Icon';
+        completeIcon.style.width = '16px';
+        completeIcon.style.marginLeft = '5px'; // 텍스트와 간격
+        this.completeBtn.node.appendChild(completeIcon);    // 버튼에 아이콘 추가
+
+        // 삭제 버튼: 아이콘 추가
+        const deleteIcon = new Image();
+        deleteIcon.src = "./assets/free-icon-delete-button-6520105.PNG";
+        deleteIcon.alt = 'Delete Icon';
+        deleteIcon.style.width = '16px';
+        deleteIcon.style.marginLeft = '5px';
+        this.delBtn.node.appendChild(deleteIcon);
     }
 
     // 만들어진 요소를 한 줄로 합쳐서 this.row에 넣고 반환
