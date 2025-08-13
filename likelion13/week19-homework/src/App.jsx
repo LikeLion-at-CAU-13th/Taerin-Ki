ef315638a2b5bcdda8b063e1b54613cac4b69067
xaerinoo <xaerinoo@naver.com>
2025-08-13 12:19:47 +0900

    week19 과제 제출

---
+++ likelion13/week19-homework/src/App.jsx
@@ -0,0 +1,13 @@
+import Component from "./Component";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>좋아하는 음식 추가하기 🍕🍔🍟🌭</h1>
+      <Component />
+    </div>
+  );
+};
+
+export default App;
